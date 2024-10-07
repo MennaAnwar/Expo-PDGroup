f47aba131ad45c278290c68c5d71a9a22a8e7ac6
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-10-07 10:56:46 +0200

    Services Overview Section

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -16,10 +16,40 @@
     <!-- END HERO SECTION -->
 
     <!-- Services Overview Section -->
-    <section class="container mt-3 services-section">
-        
+    <section class="container mt-3 p-0 services-section d-flex justify-content-between flex-wrap">
+        <div class="service-card">
+            <span>SOME OF <br>OUR WORK</span>
+        </div>
+        <div class="service-card">
+            <span>SERVICES <br> WE OFFER</span>
+        </div>
+        <div class="service-card">
+            <span>DO YOU WANT <br>TO CONTACT US</span>
+        </div>
     </section>
     <!-- End Services Overview Section -->
+
+    <!-- About Us Preview Section -->
+    <section class="container mt-3 Preview-section p-0">
+        <div class="promotion-card bg-sec d-flex position-relative">
+            <div class="d-flex flex-column justify-content-end mb-5 mb-md-0">
+                <h2 class="title text-dark">
+                    We are Redefining EXPO by creating meaningful experiences, connecting exhibitors with audiences, and driving value in creative ways.
+                </h2>
+                <div class="d-flex">
+                    <h4 class="title">WE DESIGN</h4>
+                    <div class="vr"></div>
+                    <h4 class="title">WE MANUFACTURE</h4>
+                    <div class="vr"></div>
+                    <h4 class="title">WE FIT</h4>
+                </div>
+            </div>
+            <div class="position-absolute bottom-0 end-0 p-3">
+                <button class="card-btn fs-6">READ MORE</button>
+            </div>
+        </div>
+    </section>
+    <!-- End About Us Preview Section -->
 
     <!-- Inspiration Section -->
     <section class="container bg-sec inspiration-section mt-3 p-0">
